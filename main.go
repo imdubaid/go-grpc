fd738158a3e3b151309e379c46226838c47f165c
Mohd Ubaid Khan <theubaidahmed@gmail.com>
2026-03-19 19:23:08 +0530

    Implement gRPC client and server for CoffeeShop service with menu streaming, order placement, and order status retrieval

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
 
 	pb "github.com/imdubaid/grpc/proto"
+	"google.golang.org/grpc"
 )
 
 type server struct {
@@ -31,4 +34,20 @@ func (s *server) PlaceOrder(ctx context.Context, order *pb.Order) (*pb.Receipt, 
 
 func (s *server) GetOrderStatus(ctx context.Context, receipt *pb.Receipt) (*pb.OrderStatus, error) {
 	return &pb.OrderStatus{OrderId: "123", Status: "In Progress"}, nil
+}
+
+func main() {
+	listen, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	log.Println("Server is running on port 50051")
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCoffeeShopServer(grpcServer, &server{})
+
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
 }
